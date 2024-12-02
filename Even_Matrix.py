b3e3dd8fec4d65836bf53b84421e3dec62ca2bb3
Myra Ilieva <159176371+miraIllieva@users.noreply.github.com>
2024-12-02 13:31:02 -0500

    Update Even_Matrix.py

--- Even_Matrix.py
+++ Even_Matrix.py
@@ -1,1 +1,19 @@
 
+row_count = int(input())
+
+matrix = []
+
+for i in range(row_count):
+    row_data = [int(el) for el in input().split(", ")]
+    matrix.append(row_data)
+
+
+even_matrix = []
+
+for row_index in range(len(matrix)):
+    even_matrix.append([])
+    for col_index in range(len(matrix[row_index])):
+        if matrix[row_index][col_index] % 2 == 0:
+            even_matrix[row_index].append(matrix[row_index][col_index])
+
+print(even_matrix)
